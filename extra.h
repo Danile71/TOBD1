22a05a032138ca984ac248718b750bf2a3ecea8d
hyperion11 <degterev_alex@mail.ru>
2018-01-17 11:47:43 +0700

    Add files via upload

---
+++ extra.h
@@ -0,0 +1,9 @@
+#ifndef EXTRA_H
+#define EXTRA_H
+char * TimeToString(uint32_t t)
+{
+  static char str[8];
+  sprintf(str, "%02d:%02d:%02d", t / 3600000 , (t / 60000) % 60, (t / 1000) % 60);
+  return str;
+}
+#endif
